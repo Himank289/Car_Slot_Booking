1a20ee12e407d8caebb47f59a485465541d6d693
Himank289 <tyagihimank28@gmail.com>
2024-08-12 16:11:14 +0530

    car slot image test added

--- carapp_booking_slot_with_image_test/dist/Routing/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { getUser } from "../Services/userService";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-var ProtectedRoute = function () {
-    var location = useLocation();
-    return getUser() ? (React.createElement(Outlet, null)) : (React.createElement(Navigate, { to: '/login', state: { from: location.pathname } }));
-};
-export default ProtectedRoute;
